276e9c0c8004426ff0d49fd0d32ee8dcbc9859e5
Veit Große <grosse@theva.com>
2022-01-09 14:08:50 +0100

    implemented tests if classes conform protocoll minor code style corrections

--- test_quality_data_types.py
+++ test_quality_data_types.py
@@ -1,7 +1,23 @@
 import pytest
-from quality_data_types import QualityReport, TestType
+from quality_data_types import (QualityReport, TestType, PeakInfo,
+                                AveragesInfo, ScatterInfo, QualityParameterInfo)
 
 
 def test_qualityreport_init():
     report = QualityReport("my_id", TestType.AVERAGE, [])
     assert report.fail_information is None
+
+
+def test_peakinfo_conforms_protocoll():
+    info = PeakInfo()
+    assert isinstance(info, QualityParameterInfo)
+
+
+def test_averagesinfo_conforms_protocoll():
+    info = AveragesInfo()
+    assert isinstance(info, QualityParameterInfo)
+
+
+def test_scatterinfo_conforms_protocoll():
+    info = ScatterInfo()
+    assert isinstance(info, QualityParameterInfo)
